1f479d6f8e6e37e5456e89c96afb24a51606524f
Kevin Ahn <seungseopahn@gmail.com>
2024-10-02 12:46:51 +1300

    fix: advertisement view history repository

--- src/main/java/com/example/board/repository/AdvertisementViewHistoryRepository.java
+++ src/main/java/com/example/board/repository/AdvertisementViewHistoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdvertisementViewHistoryRepository extends MongoRepository<AdvertisementViewHistory, Long> {
+public interface AdvertisementViewHistoryRepository extends MongoRepository<AdvertisementViewHistory, String> {
 }
